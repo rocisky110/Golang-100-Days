02607893f97aac457ed741f747606383c41fbfe0
rocisky110 <rocisky@yeah.net>
2023-05-17 17:06:55 +0800

    11

--- !_MyGoCodes/archive_zip/ReadZIPfiles.go
+++ !_MyGoCodes/archive_zip/ReadZIPfiles.go
@@ -3,9 +3,9 @@ package main
 import (
 	"archive/zip"
 	"fmt"
-	"io"
 	"log"
-	"os"
+	"unicode/utf8"
+    "github.com/axgle/mahonia"
 )
 
 func main() {
@@ -19,16 +19,7 @@ func main() {
 	// 遍历存档中的文件，
 	// 打印他们的一些内容。
 	for _, f := range r.File {
+		data := ConvertToString(str, "gbk", "utf-8")
 		fmt.Printf("Contents of %s:\n", f.Name)
-		rc, err := f.Open()
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = io.CopyN(os.Stdout, rc, 68)
-		if err != nil {
-			log.Fatal(err)
-		}
-		rc.Close()
-		fmt.Println()
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.20
+
+require github.com/fragmentization/mahonia v0.0.0-20220819055008-8fcf21ccb869 // indirect
