acec36bffb0b1541689b7ce93385a4ad6cf27440
rocisky110 <rocisky@yeah.net>
2023-05-17 17:23:41 +0800

    11

--- !_MyGoCodes/archive_zip/ReadZIPfiles.go
+++ !_MyGoCodes/archive_zip/ReadZIPfiles.go
@@ -5,8 +5,21 @@ import (
 	"fmt"
 	"log"
 	"unicode/utf8"
-    "github.com/axgle/mahonia"
+
+	"github.com/fragmentization/mahonia"
 )
+
+func ConvertToString(src string, srcCode string, tagCode string) string {
+	if utf8.Valid([]byte(src)) { //判断src是否本身就是utf8
+		return src
+	}
+	srcCoder := mahonia.NewDecoder(srcCode)
+	srcResult := srcCoder.ConvertString(src)
+	tagCoder := mahonia.NewDecoder(tagCode)
+	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
+	result := string(cdata)
+	return result
+}
 
 func main() {
 	// 打开一个zip文件供阅读。
@@ -16,10 +29,9 @@ func main() {
 	}
 	defer r.Close()
 
-	// 遍历存档中的文件，
-	// 打印他们的一些内容。
+	//中文文件名GBK乱码转UTF-8
 	for _, f := range r.File {
-		data := ConvertToString(str, "gbk", "utf-8")
-		fmt.Printf("Contents of %s:\n", f.Name)
+		f_Name := ConvertToString(f.Name, "gbk", "utf-8")
+		fmt.Printf("Contents of %s:\n", f_Name)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module main
 
 go 1.20
 
-require github.com/fragmentization/mahonia v0.0.0-20220819055008-8fcf21ccb869 // indirect
+require (
+	github.com/fragmentization/mahonia v0.0.0-20220819055008-8fcf21ccb869 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/redis/go-redis v6.15.9+incompatible // indirect
+)
